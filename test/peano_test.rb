58123964155c1f242463c07b375c784d6595b48c
Frank Shearar <frank.shearar@gmail.com>
2011-11-26 16:53:54 +0000

    QuickCheck-style polymorphic generator = virtual method.

--- test/peano_test.rb
+++ test/peano_test.rb
@@ -13,13 +13,21 @@ class Rantly
     limit = 0..Peano::MAX_INT if limit.nil?
     Peano.from_i(integer(limit))
   end
+
+module Peano
+  class PNumber
+    def self.generator(limit = nil)
+      limit = 0..Peano::MAX_INT if limit.nil?
+      Peano.from_i(Rantly.gen.integer(limit))
+    end
+  end
 end
 
 module Peano
   describe "Peano" do
     it "should have Zero identifiable" do
       property_of {
-        peano
+        PNumber.generator
       }.check {|i|
         i.zero?.should == i.kind_of?(Zero)
       }
@@ -27,7 +35,7 @@ module Peano
 
     it "should 0 < n" do
       property_of {
-        peano(1..100)
+        PNumber.generator(1..100)
       }.check { |n|
         Peano.zero.should < n
       }
@@ -43,13 +51,13 @@ module Peano
 
     it "should succ(pred(n)) == n" do
       property_of {
-        peano(0..Peano::MAX_INT - 1)
+        PNumber.generator(0..Peano::MAX_INT - 1)
       }.check {|n|
         n.succ.pred.should == n
       }
     end
 
-    it "should pred(n) < n" do
+    it "should pred(n) < n like integer's <" do
       property_of {
         i = integer(1..Peano::MAX_INT)
         [i, i-1].map{|n| Peano.from_i(n)}
@@ -58,12 +66,28 @@ module Peano
       }
     end
 
-    it "should succ(n) > n" do
+    it "should pred(n) < n" do
+      property_of {
+        PNumber.generator(1..Peano::MAX_INT)
+      }.check {|n|
+        n.pred.should < n
+      }
+    end
+
+    it "should succ(n) > n like integer's >" do
       property_of {
         i = range(0, Peano::MAX_INT - 1)
-        [i, i+1].map{|n| Peano.from_i(n)} 
+        [i, i+1].map{|n| Peano.from_i(n)}
       }.check { |n, succ_n|
          n.should < succ_n
+      }
+    end
+
+    it "should succ(n) > n" do
+      property_of {
+        PNumber.generator(0..Peano::MAX_INT - 1)
+      }.check {|n|
+        n.should < n.succ
       }
     end
 
@@ -78,7 +102,7 @@ module Peano
 
     it "should have Zero as the additive identity" do
       property_of {
-        i = peano(0..Peano::MAX_INT - 1)
+        i = PNumber.generator(0..Peano::MAX_INT - 1)
         [i, i + Zero.new, Zero.new + i]
       }.check {|i, i_plus_zero, zero_plus_i|
         i.should == i_plus_zero
@@ -91,8 +115,8 @@ module Peano
 
     it "should define + like the integers" do
       property_of {
-        i = peano(0..Peano::MAX_INT / 2)
-        j = peano(0..Peano::MAX_INT / 2)
+        i = PNumber.generator(0..Peano::MAX_INT / 2)
+        j = PNumber.generator(0..Peano::MAX_INT / 2)
         [i, j]
       }.check {|i, j|
         (i + j).to_i.should == (i + j)
@@ -102,7 +126,7 @@ module Peano
     it "should reduce an array of Peano numbers" do
       property_of {
         sized(integer(1..10)) {
-          array { peano(0..Peano::MAX_INT / 10) }
+          array { PNumber.generator(0..Peano::MAX_INT / 10) }
         }
       }.check {|numbers|
         numbers.reduce(:+).should == (numbers.map{|p| p.to_i}.reduce(:+))
